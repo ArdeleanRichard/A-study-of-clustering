778e01d9d15679fca09cd6ee388643545acfc9cf
ralucaportase <raluca@sectorlabs.ro>
2025-01-24 20:26:01 +0200

    Use dipNSub and projectedDipMeans

--- gs_algos.py
+++ gs_algos.py
@@ -1,5 +1,5 @@
 from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering, SpectralClustering, MeanShift, Birch, OPTICS, HDBSCAN, AffinityPropagation
-from clustpy.partition import DipInit
+from clustpy.partition import DipInit, DipNSub, ProjectedDipMeans
 
 from clustering_algos.DRLDBSCAN.main import DrlDbscanAlgorithm
 from clustering_algos.autoclustering import AutoClustering
@@ -87,12 +87,31 @@ def load_algorithms():
         #    },
         #},
 
-         "dipInit": {
-             "estimator": DipInit,
-             "param_grid": {
-                 "n_clusters": [2, 3, 4, 5],
-             },
-         },
+        # "dipInit": {
+        #     "estimator": DipInit,
+        #     "param_grid": {
+        #         "n_clusters": [2, 3, 4, 5],
+        #         "dip_threshold": [0.1, 0.25, 0.5, 0.75, 1],
+        #     },
+        # },
+        # NEEDS Data to be 1-dimensional
+        "dipNSub": {
+            "estimator": DipNSub,
+            "param_grid": {
+                "outliers": [True, False],
+                "consider_duplicates": [True, False],
+                "threshold": [0.05, 0.1, 0.15, 0.2, 0.5, 0.1],
+                "significance": [0.05, 0.1, 0.2, 0.4, 0.8, 1],
+            },
+        },
+        # "projectedDipMeans": {
+        #     "estimator": ProjectedDipMeans,
+        #     "param_grid": {
+        #         "significance": [0.0005, 0.001, 0.005, 0.01],
+        #         "pval_strategy": ["table", "bootstrap"],
+        #         "n_split_trials": [5, 10, 15, 20],
+        #     },
+        # },
 
     }
     return algorithms
